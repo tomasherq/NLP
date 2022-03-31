5ee316ee93a1a06a9e07828289c4b87d9b84833d
tomasherq <T.HernandezQuintanilla@student.tudelft.nl>
2022-03-31 14:37:56 +0200

    classifier script

--- classify_clickbait.py
+++ classify_clickbait.py
@@ -2,23 +2,53 @@ from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipe
 import os
 import json
 
+
+def load_json_file(filename):
+    with open(filename, "r") as file_read:
+
+        return json.load(file_read)
+
+
+def write_json(filename, dict_to_print):
+    with open(filename, "w") as file_write:
+
+        return file_write.write(json.dumps(dict_to_print, indent=4))
+
+
 tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
 model = AutoModelForSequenceClassification.from_pretrained("models/predict")
 MAX_LENGTH = 20
 
 classifier = pipeline("text-classification", model=model, tokenizer=tokenizer)
 
-with open("classified_phrases.json", "r") as file_read:
+classifiedFile = f"memory/classified_phrases_{MAX_LENGTH}.json"
+classifiedPhrases = []
+if os.path.exist(classifiedFile):
 
-    classifiedPhrases = json.load(file_read)
-
-
-os.listdir(f"output_phrases/{MAX_LENGTH}")
-
-
-result = classifier("Meet the happiest #dog in the world!")
-result2 = classifier("Tokyo's subway is shut down amid fears over an imminent North Korean missile attack on Japan")
+    classifiedPhrases = load_json_file(classifiedFile)
 
 
-print(result)
-print(result2)
+labelsFile = f"results/labels_{MAX_LENGTH}.json"
+labels = []
+if os.path.exist(labelsFile):
+
+    labels = load_json_file(labelsFile)
+
+phrasesDirectory = f"output_phrases/{MAX_LENGTH}"
+for file in os.listdir(phrasesDirectory):
+
+    if file not in classifiedPhrases:
+        file_location = phrasesDirectory+"/"+file
+        sentences = load_json_file(file_location)
+        for sentence in sentences:
+            result = classifier("Meet the happiest #dog in the world!")
+            label = 1
+            if "0" in result["label"]:
+                label = 0
+
+            labels.append(label)
+
+        classifiedPhrases.append(file)
+
+        write_json(classifiedFile, classifiedPhrases)
+        write_json(labelsFile, labels)
